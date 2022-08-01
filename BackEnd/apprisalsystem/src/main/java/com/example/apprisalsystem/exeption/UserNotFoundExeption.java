b4dfad835f946ffe10ba61620a9dcb6c1f73435c
Saravanan S <77256311+Saravanan1501@users.noreply.github.com>
2022-08-01 22:35:59 +0530

    Delete BackEnd directory

--- BackEnd/apprisalsystem/src/main/java/com/example/apprisalsystem/exeption/UserNotFoundExeption.java
+++
@@ -1,7 +0,0 @@
-package com.example.apprisalsystem.exeption;
-
-public class UserNotFoundExeption extends RuntimeException{
-    public UserNotFoundExeption(String message) {
-        super(message);
-    }
-}
